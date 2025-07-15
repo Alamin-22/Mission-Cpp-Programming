73860e009c2a09e2a25820e99b7a267797893776
Alamin-22 <mdalaminmollick@gmail.com>
2025-07-15 22:22:22 +0600

    solved tringler pattern

--- Basic-About-Cpp/Palindrome.cpp
+++ Basic-About-Cpp/Palindrome.cpp
@@ -10,6 +10,18 @@ int main()
     int sizeOfWord = word.length();
     bool isPalindrome = true;
 
+    /*
+
+       m   a   d   a   m
+    [0] [1] [2] [3] [4]
+
+    | `i` | `name[i]` | `name[lengthOfName - 1 - i]` | Comparison    |
+    | --- | --------- | ---------------------------- | ------------  |
+    | 0   | `'m'`     | `name[5 - 1 - 0] = name[4]`  | `'m' == 'm'`  |
+    | 1   | `'a'`     | `name[5 - 1 - 1] = name[3]`  | `'a' == 'a'`  |
+    | 2   | `'d'`     | `name[5 - 1 - 2] = name[2]`  | `'d' == 'd'`  |
+    */
+
     for (int i = 0; i < sizeOfWord / 2; i++)
     {
         if (word[i] != word[sizeOfWord - 1 - i])
